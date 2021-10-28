a828c557857f6ebe13610e8d1a412de482857c7a
KaeCal <86895892+KaeCal@users.noreply.github.com>
2021-10-28 16:42:02 -0400

    Create grade.js

---
+++ grade.js
@@ -0,0 +1,11 @@
+table.read(function (context) {
+  return context.execute();
+});
+
+table.insert(function (context) {
+  return context.execute();
+});
+
+table.update(function (context) {
+  return context.execute();
+});
